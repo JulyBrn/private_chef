dae0f2a9e65c151b50a81e917cd2a2dcc5342e7f
July <july.brisson@wmki.fr>
2025-07-01 18:07:51 +0200

    re organisation admin front

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -2,16 +2,44 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Article;
+use App\Form\ArticleForm;
+use App\Repository\ArticleRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 final class AdminController extends AbstractController
 {
   #[Route('/admin', name: 'admin')]
-  public function index(): Response
+  public function index(ArticleRepository $repository): Response
   {
+    $publications = $repository->findAll();
+
     // $this->denyAccessUnlessGranted('ROLE_USER');
-    return $this->render('admin/admin.html.twig');
+    return $this->render('admin/admin.html.twig',[
+      'publications' => $publications
+    ]);
+  }
+
+   #[Route('/admin/edit-{id}', name:"update")]
+  public function update(Article $article, Request $request, EntityManagerInterface $em)
+  {
+    $form = $this->createForm(ArticleForm::class, $article);
+    $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid())
+    {
+      $em->flush();
+      $this->addFlash('success','La publication a bien été modifiée.');
+      return $this->redirectToRoute('publication');
+    }
+
+    return $this->render('admin/publication-update.html.twig',[
+      'article' => $article,
+      'form' => $form,
+      ]);
   }
 }
