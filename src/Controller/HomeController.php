7e1b165ab2662f65cffd21b2092f801c1d4d7160
July <july.brisson@wmki.fr>
2025-07-21 18:15:48 +0200

    admin

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,28 +3,31 @@
 namespace App\Controller;
 
 use App\Entity\Prospect;
+use App\Entity\User;
 use App\Form\ContactForm;
 use App\Repository\ArticleRepository;
+use DateTime;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+
+use function Symfony\Component\Clock\now;
 
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(Request $request,  MailerInterface $mailer, EntityManagerInterface $entityManager, ArticleRepository $article): Response
+    public function index(Request $request,  MailerInterface $mailer, EntityManagerInterface $entityManager, ArticleRepository $article, UserPasswordHasherInterface $haser): Response
     {
         $contacts = new Prospect();
         $form = $this->createForm( ContactForm::class , $contacts);
         $form->handleRequest($request);
-
         if($form->isSubmitted() && $form->isValid())
         {
-            $datas = $form->getData();
             // Envoi de l'email
             $emailtosend = (new TemplatedEmail())
                 ->from($contacts->getEmail())
@@ -33,7 +36,8 @@ final class HomeController extends AbstractController
                 ->htmlTemplate('emails/contact.html.twig')
                 ->context([ 'data' => $contacts ]);
             $mailer->send($emailtosend);
-            
+
+            $contacts->setDate(new DateTime('now'));
             // Enregistrement dans la base de données
             $entityManager->persist($contacts);
             $entityManager->flush();
--- src/Form/ContactForm.php
+++ src/Form/ContactForm.php
@@ -3,6 +3,7 @@
 namespace App\Form;
 
 use App\Entity\Prospect;
+use DateTime;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
