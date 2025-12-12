a2d3a9fee52fba40fc0af75e832ae7e82e2995a1
July <july.brisson@wmki.fr>
2025-12-12 12:44:42 +0100

    secure admin

--- src/Controller/Admin/AdminController.php
+++ src/Controller/Admin/AdminController.php
@@ -10,9 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-
-// #[IsGranted('ROLE_USER')]
+#[IsGranted('ROLE_USER')]
 final class AdminController extends AbstractController
 {
   #[Route('/admin', name: 'admin')]
